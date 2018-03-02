434c16ce1940234806ab4b6977c6e049015db98d
Mahender Singh D <mahenderd@Hyd.NetwinSolutions.pri>
2018-03-02 07:58:35 +0530

    First repo

--- src/main/java/com/konnect/util/TestUtil.java
+++ src/main/java/com/konnect/util/TestUtil.java
@@ -52,7 +52,8 @@ public class TestUtil extends TestBase {
 		}
 		sheet = book.getSheet(sheetName);
 		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-		 //System.out.println(sheet.getLastRowNum() + "--------" +
+		// 
+		//System.out.println(sheet.getLastRowNum() + "--------" +
 		// sheet.getRow(0).getLastCellNum());
 		for (int i = 0; i < sheet.getLastRowNum(); i++) {
 			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
